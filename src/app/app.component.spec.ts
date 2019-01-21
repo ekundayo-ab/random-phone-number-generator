baab13459337cf29a5b93fc0db9a329d7c9de446
Ekundayo Abiona <ekundayoabiona@gmail.com>
2019-01-21 15:31:24 +0100

    Add functionality for generating random numbers with tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,90 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture, tick, fakeAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ReactiveFormsModule]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rpng'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('rpng');
-  });
+  it('should generate phone numbers after page loads', fakeAsync(() => {
+    component.ngOnInit();
+    tick(100);
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to rpng!');
-  });
+    expect(component.phoneNumbers.length).toBeTruthy();
+  }));
+
+  it('should generate phone numbers according to amount', fakeAsync(() => {
+    component.ngOnInit();
+    tick(100);
+
+    const amountOfNumbers = 10;
+    component.numberForm.patchValue({ amountOfNumbers });
+
+    const element = fixture.debugElement;
+
+    const generateBtn = element.query(By.css('.has-addons button'));
+    generateBtn.nativeElement.click();
+    tick(100);
+    expect(component.phoneNumbers.length).toEqual(amountOfNumbers);
+  }));
+
+  it('should not generate phone numbers if maximum amount is exceeded', fakeAsync(() => {
+    component.numberForm.patchValue({ amountOfNumbers: 10005 });
+    component.ngOnInit();
+    tick(100);
+    expect(component.phoneNumbers.length).toBeFalsy();
+  }));
+
+  it('should sort numbers based on selected sort field', fakeAsync(() => {
+    const element = fixture.debugElement;
+
+    component.numberForm.patchValue({ amountOfNumbers: 2, sortState: 'asc' });
+    component.ngOnInit();
+    tick(100);
+
+    const ascendingSortFirst = component.phoneNumbers[0];
+
+    component.numberForm.patchValue({ sortState: 'desc' });
+    const select = element.query(By.css('select')).nativeElement;
+    select.value = select.options[0].value;
+    select.dispatchEvent(new Event('change'));
+
+    const descendingSortFirst = component.phoneNumbers[0];
+
+    expect(ascendingSortFirst).not.toEqual(descendingSortFirst);
+  }));
+
+  it('should call saveNumbers when Save list to file button is clicked', fakeAsync(() => {
+    component.ngOnInit();
+    tick(100);
+
+    const element = fixture.debugElement;
+    spyOn(component, 'saveNumbers');
+
+    element.query(By.css('.save button')).nativeElement.click();
+
+    expect(component.saveNumbers).toHaveBeenCalled();
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rpng';
+export class AppComponent implements OnInit {
+  phoneNumbers: number[] = [];
+  maxAmount = 10000;
+  sortState = 'asc';
+
+  generatingNumbers = false;
+  maxAmountExceeded = false;
+
+  numberForm: FormGroup;
+
+  constructor() {
+    this.numberForm = new FormGroup({
+      amountOfNumbers: new FormControl(100),
+      sortState: new FormControl('asc'),
+    });
+  }
+
+  ngOnInit() {
+    this.generateNumbers();
+  }
+
+  generateNumbers() {
+    this.setFormState();
+    const { amountOfNumbers } = this.numberForm.value;
+
+    if (amountOfNumbers > this.maxAmount) {
+      this.setFormState(false, true);
+      return;
+    }
+
+    const phoneDigitsLength = 1000000000;
+    const randomMin = 0.1;
+    const randomMax = 1;
+
+    const phoneNumbers = [];
+
+    setTimeout(() => {
+      for (let i = 0; i < amountOfNumbers; i += 1) {
+        const randomNumber = Math.random() * (randomMax - randomMin) + randomMin;
+        const numberToAdd = Math.floor(randomNumber * phoneDigitsLength);
+
+        phoneNumbers.push(Math.floor(numberToAdd));
+      }
+
+      this.phoneNumbers = phoneNumbers;
+      this.sortNumbers();
+
+      this.setFormState(false, false);
+    }, 100);
+  }
+
+  sortNumbers() {
+    const { sortState } = this.numberForm.value;
+
+    if (sortState === 'asc') {
+      this.phoneNumbers.sort();
+    } else {
+      this.phoneNumbers = this.phoneNumbers.reverse();
+    }
+  }
+
+  setFormState(generatingNumbers = true, maxAmountExceeded?) {
+    this.generatingNumbers = generatingNumbers;
+    this.maxAmountExceeded = maxAmountExceeded;
+  }
+
+  saveNumbers() {
+    const blob = new Blob(
+      this.phoneNumbers.map(number => `0${number.toString()}\n`),
+      {type: 'text/plain;charset=utf-8'}
+    );
+
+    if (this.phoneNumbers.length) {
+      return FileSaver.saveAs(blob, 'phone-numbers.txt');
+    }
+  }
 }
